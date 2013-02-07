b32a1d2deca9cdeff234d25eb6c1e46ba1ba860f
gvirav <gvirav@gmail.com>
2013-02-07 14:30:24 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Spotifind::Application.config.secret_token = '64acdc8ee204d55907038daf2117e571e420964837a70263996c1bd503ce229ce24276a6bc495b192b58fb35beb2c43feffd792d7c0e45878ad9f33fd2ae7f7f'
